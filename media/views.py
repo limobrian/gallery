aa4fffbfe8197ae11599b05bcb8c91b0500638e1
limooh <limobrian290@gmail.com>
2019-08-25 17:42:09 +0300

    formed a function that will get data from gallery template

--- media/views.py
+++ media/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django import HttpResponse, Http404
+from .models import image
 
 # Create your views here.
+
+
+def gallery (request):
+    all_pic = Image.all_pics()
+    print(all_pic)
+    return render(request,'gallery.html',{"all_pic":all_pic})
+
