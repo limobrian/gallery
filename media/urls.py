19f97ac56363cae19537a00ab98dd90f45c17364
limooh <limobrian290@gmail.com>
2019-08-25 18:16:22 +0300

    created urls.py file that will be map functions form views request

---
+++ media/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.gallery,name = 'gallery'),
+    url(r'^categories/',views.display_images_categories,name = 'categories'),
+    url(r'^locations/',views.display_images_locations,name= 'locations'),
+    url(r'^search/',views.search_results,name = 'search_results')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
