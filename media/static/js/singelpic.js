01605c70af7ee34f5f6fce3be5dd1ce71f19b24f
limooh <limobrian290@gmail.com>
2019-08-25 22:45:10 +0300

    dsingelpic js files

---
+++ media/static/js/singelpic.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('button#copylink').on('click', function () {
+        // Get the text field
+        var copyLink = $('input#imageurl')
+
+        // Select the text field
+        copyLink.select()
+
+        // Copy the text inside the text field
+        document.execCommand('Copy')
+
+        // Alert the copied text
+        alert('Link Copied!')
+    })
+})
